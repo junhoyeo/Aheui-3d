eef3c66a408bc0449ebea7513555cf8b0823867d
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-07-10 16:19:04 +0900

    프로젝트 초기화

---
+++ README.md
@@ -0,0 +1,3 @@
+# Aheui 3D
+
+[Aheui](https://aheui.readthedocs.io/en/latest/specs.en.html) editor with 3D visualization
---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Aheui editor with 3D visualization
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
